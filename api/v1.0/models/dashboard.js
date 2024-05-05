7f60a77f433d941186955991be40a532aa7f4279
Phipattanachai <>
2024-05-05 16:04:32 +0700

    no message

--- api/v1.0/models/dashboard.js
+++ api/v1.0/models/dashboard.js
@@ -6,16 +6,16 @@ const ListDashboard = function () {
     return new Promise(async (resolve, reject) => {
         const client = await connectionR.connect();
         try {
-            var sqlQuery = `SELECT 'Company' AS table_name, COUNT(company_id) AS count_result
+            var sqlQuery = `SELECT 'Partner registered' AS table_name, COUNT(company_id) AS count_result
             FROM company
             UNION ALL
             SELECT 'Active' AS table_name, COUNT(company_id) AS count_result
             FROM company WHERE company_status = 1
             UNION ALL
-            SELECT 'InActive' AS table_name, COUNT(company_id) AS count_result
+            SELECT 'Inactive' AS table_name, COUNT(company_id) AS count_result
             FROM company WHERE company_status = 0
             UNION ALL
-            SELECT 'Contact' AS table_name, COUNT(contact_id) AS count_result
+            SELECT 'Contact User' AS table_name, COUNT(contact_id) AS count_result
             FROM company_contact`;
             let rows = await client.query(sqlQuery);
             if (rows.rows) {
